ebcdb6edb0ccb2223da62200af5fdf442c8bd0ee
Matt <mattmccoy94@gmail.com>
2016-02-04 11:13:01 -0500

    initial commit

---
+++ app/src/main/java/com/team19/gtmovies/WelcomeActivity.java
@@ -0,0 +1,22 @@
+package com.team19.gtmovies;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+    public void userLogin(View view) {
+        Intent login = new Intent(this, LoginActivity.class);
+        startActivity(login);
+        finish();
+    }
+}
