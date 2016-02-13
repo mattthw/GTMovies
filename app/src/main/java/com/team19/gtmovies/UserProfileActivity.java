b81c48dafad3c527e19e8f4a68da7f69e73fe0c1
Shayan Pirani <shayanpirani@gmail.com>
2016-02-13 18:35:38 -0500

    Pre-fill the username, password, and name fields in the user profile.

--- app/src/main/java/com/team19/gtmovies/UserProfileActivity.java
+++ app/src/main/java/com/team19/gtmovies/UserProfileActivity.java
@@ -2,6 +2,7 @@ package com.team19.gtmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        ((EditText)findViewById(R.id.editTextUserProfileUsername)).setText(MainActivity.ioa.currentUser.getUsername());
+        ((EditText)findViewById(R.id.editTextUserProfileName)).setText(MainActivity.ioa.currentUser.getName());
+        ((EditText)findViewById(R.id.editTextUserProfilePassword)).setText(MainActivity.ioa.currentUser.getPassword());
     }
 }
