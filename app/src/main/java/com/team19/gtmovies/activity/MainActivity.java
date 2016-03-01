7a97dee78e221a9f9db8cb0c67c0d2d29516a17c
Jinu Jang <jimjang95@gmail.com>
2016-03-01 18:18:44 -0500

    Austin Fatal Error fix

--- app/src/main/java/com/team19/gtmovies/activity/MainActivity.java
+++ app/src/main/java/com/team19/gtmovies/activity/MainActivity.java
@@ -152,7 +152,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onPageScrolled(int position, float positionOffset,
                                        int positionOffsetPixels) {
-                onPageSelected(position - 1);
+                onPageSelected(position);
             }
 
             @Override
