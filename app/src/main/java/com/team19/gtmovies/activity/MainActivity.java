fe3333940bb9ac8e704884913b990a2c4bc56c3e
Austin Leal <bobzeka@gmail.com>
2016-03-06 18:55:06 -0500

    Replaced deprecated method setDrawerListener in MainActivity

--- app/src/main/java/com/team19/gtmovies/activity/MainActivity.java
+++ app/src/main/java/com/team19/gtmovies/activity/MainActivity.java
@@ -81,7 +81,7 @@ public class MainActivity extends AppCompatActivity
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         // Layout navigation
