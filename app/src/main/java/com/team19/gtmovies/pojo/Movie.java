d841506e2117cceca96666a117d6f144a2feef3c
Matt <mattmccoy94@gmail.com>
2016-04-04 13:44:18 -0400

    javadocs and small fix

--- app/src/main/java/com/team19/gtmovies/pojo/Movie.java
+++ app/src/main/java/com/team19/gtmovies/pojo/Movie.java
@@ -17,6 +17,8 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -165,6 +167,15 @@ public class Movie implements Comparable<Movie>, Serializable {
         }
     }
 
+    /**
+     * set myReviews with list of reviews form database
+     */
+    public void setReviews() {
+        ArrayList<Review> list = IOActions.getListMovieReviews(getID());
+        for (Review r: list) {
+            this.addReview(r);
+        }
+    }
     /**
      * get map of all reviews for movie
      * @return hashmap of reviews
